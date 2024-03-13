724b24d1cbc754ea894a4aad71e1e195ffc1728a
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-03-13 12:14:02 +0900

    ボタンの作成(コンポーネント使用)

---
+++ todo_app/src/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button clasName="ui basic button">
+      <i class="icon user" />
+        友達を追加 
+    </button>
+  )
+}
+
+export default Button
