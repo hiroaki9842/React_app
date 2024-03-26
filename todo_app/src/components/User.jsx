dd0be872b41c19727d16c2e02af457304702f0ab
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-03-26 11:20:57 +0900

    ユーザー画面のデザイン作成

--- todo_app/src/components/User.jsx
+++ todo_app/src/components/User.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './User.css';
+import './Post.css';
 
 const User = () => {
   return (
-    <div className="main">
-      ユーザー画面
+  <div className="main">
+    <div className="user-main">
+      <div className="user-icon">
+        アイコン(仮)
+      </div>
+      <div className="user-profile">
+        プロフィール(仮)
+      </div>
+      <div className="user-message">
+        メッセージ(仮)
+      </div>
     </div>
+  </div>
   )
 }
 
