c60d576875eec90e0fc2b8c3b4161588ec960c2c
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-03-26 18:16:02 +0900

    ブログ一覧の各コンテンツデザイン作成

--- todo_app/src/components/BlogList.jsx
+++ todo_app/src/components/BlogList.jsx
@@ -4,7 +4,32 @@ import './BlogList.css';
 const BlogList = () => {
   return (
     <div className="main">
-      投稿一覧
+      <div className="list-main">
+        <div className="content-main">
+          <div className="content-title">
+            タイトル
+          </div>
+          <div className="content">
+            内容
+          </div>
+        </div>
+        <div className="date-user-container">
+          <div className="post-day">
+            2024年3月26日(仮)
+          </div>
+          <div className="post-user">
+            ユーザー名(仮)
+          </div>
+        </div>
+        <div className="button">
+          <button className="edit-button" class="ui green button">
+            編集
+          </button>
+          <button className="delete-button" class="ui grey button">
+            削除
+          </button>
+        </div>
+      </div>      
     </div>
   )
 }
