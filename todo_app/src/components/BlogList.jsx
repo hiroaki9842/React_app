91d92125841d025df82786adca2c83dbd95c74ff
杉本博陽 <hiroaki.sugimoto@glv.co.jp>
2024-03-27 11:24:57 +0900

    投稿一覧のデザイン修正

--- todo_app/src/components/BlogList.jsx
+++ todo_app/src/components/BlogList.jsx
@@ -5,13 +5,43 @@ import './BlogList.css';
 const BlogList = () => {
   return (
     <div className="main">
+{/* 各ブログコンテンツ開始 */}
       <div className="list-main">
         <div className="content-main">
           <div className="content-title">
-            タイトル
+            タイトル(仮)
           </div>
           <div className="content">
-            内容
+            内容(仮)
+          </div>
+        </div>
+        <div className="date-user-container">
+          <div className="post-day">
+            2024年3月26日(仮)
+          </div>
+          <div className="post-user">
+            ユーザー名(仮)
+          </div>
+        </div>
+        <div className="button">
+          <Link to="/revise" className="ui green button">
+            編集
+          </Link>
+          <Link to="/delete" className="ui grey button">
+            削除
+          </Link>
+        </div>
+      </div>      
+      <br></br>
+{/* 各ブログコンテンツ終了 */}
+{/* 各ブログコンテンツ開始 */}
+  <div className="list-main">
+        <div className="content-main">
+          <div className="content-title">
+            タイトル(仮)
+          </div>
+          <div className="content">
+            内容(仮)
           </div>
         </div>
         <div className="date-user-container">
@@ -31,6 +61,8 @@ const BlogList = () => {
           </Link>
         </div>
       </div>      
+      <br></br>
+{/* 各ブログコンテンツ終了 */}
     </div>
   )
 }
